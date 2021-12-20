6b8fbc7f31e52d12fe5ec34d67954ddf889aac10
jeong <jmih43@gmail.com>
2021-12-20 14:34:10 +0900

    mybatis

---
+++ mybatisWork02/src/com/test/mybatis/MemberMain.java
@@ -0,0 +1,33 @@
+/*===================
+   MemberMain.java
+===================*/
+
+package com.test.mybatis;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class MemberMain
+{
+	// mybatis 객체 의존성 (자동) 주입~!!!
+	@Autowired
+	private SqlSession sqlSession;
+	
+	@RequestMapping(value = "/memberlist.action", method = RequestMethod.GET)
+	public String memberList(ModelMap model)
+	{
+		//IMemberDAO dao = (IMemberDAO) new IMemberDAO();
+		IMemberDAO dao = sqlSession.getMapper(IMemberDAO.class);	//@ 업캐스팅... 등록해둔 bean 들이 돌아간다
+		
+		model.addAttribute("count", dao.count());
+		model.addAttribute("list", dao.list());
+		
+		return "WEB-INF/view/MemberList.jsp";
+	}
+	
+}
